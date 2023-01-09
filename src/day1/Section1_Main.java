68594ad175afc4a266fbbb779173cad12c84c49b
kd0427 <rlaemek@naver.com>
2023-01-09 23:26:21 +0900

    2023 함수형프로그래밍 스터디

---
+++ src/day1/Section1_Main.java
@@ -0,0 +1,17 @@
+package day1;
+
+import day1.util.Adder;
+
+import java.util.function.Function;
+
+public class Section1_Main {
+    public static void main(String[] args) {
+        Function<Integer, Integer> adder = new Adder();
+
+        System.out.println(adder.apply(10));
+
+        // 람다식으로 구현
+        Function<Integer, Integer> adder2 = x->x+10;
+        System.out.println(adder2.apply(10));
+    }
+}
