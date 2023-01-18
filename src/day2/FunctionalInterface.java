18d32017c56caaa0e8a7ad10607924890af386aa
kd0427 <rlaemek@naver.com>
2023-01-18 23:35:57 +0900

    2023 함수형프로그래밍 스터디

--- src/day2/FunctionalInterface.java
+++ src/day2/FunctionalInterface.java
@@ -3,6 +3,7 @@ package day2;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -26,10 +27,20 @@ public class FunctionalInterface {
             System.out.println(x*2); ;
         };
         process(IntegerInputs,a);
+
+        List<Integer> IntegerInputs2 = Arrays.asList(2,4,6,8,10);
+        BiConsumer<Integer,Integer> b = (x,y)->{
+            System.out.println(x+y);
+        };
     }
     public static void process(List<Integer> inputs, Consumer<Integer> processor){
         for(Integer input : inputs){
             processor.accept(input);
+        }
+    }
+    public static void process2(List<Integer> inputs, BiConsumer<Integer,Integer> processor){
+        for(Integer input : inputs){
+            processor.accept(input,input);
         }
     }
 }
