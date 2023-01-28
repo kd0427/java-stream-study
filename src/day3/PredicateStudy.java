9ec47f5d7347691c6de5168820bd7c33206c18a9
kd0427 <rlaemek@naver.com>
2023-01-28 23:58:33 +0900

    2023 함수형프로그래밍 스터디

--- src/day3/PredicateStudy.java
+++ src/day3/PredicateStudy.java
@@ -6,6 +6,9 @@ public class PredicateStudy {
     public static void main(String[] args) {
         Predicate<Integer> isPositive = x->x>1;
 
+        Predicate<String> auth = "관리자"::equals;
+
         System.out.println(isPositive.test(-10));
+        System.out.println(auth.test("손님"));
     }
 }
