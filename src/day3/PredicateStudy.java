5acd03d4ee1ead48a379a0cf19a5f4a636be2659
kd0427 <rlaemek@naver.com>
2023-02-21 22:33:37 +0900

    2023 함수형프로그래밍 스터디

--- src/day3/PredicateStudy.java
+++ src/day3/PredicateStudy.java
@@ -21,6 +21,7 @@ public class PredicateStudy {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+        list.add(3);
         System.out.println(listSize.test(list));
     }
 }
